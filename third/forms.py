87acba93be81b86095b5c7d19768bd7e3bd58c3e
suhyeon_Lee <81307924+suhyeon0111@users.noreply.github.com>
2021-09-15 00:11:14 +0900

    django relation 리뷰리스트 구현

--- third/forms.py
+++ third/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
-from third.models import Restaurant
+from django import forms
+from third.models import Restaurant, Review
 from django.utils.translation import gettext_lazy as _
 
+REVIEW_POINT_CHOICES = (
+    ('1', 1),
+    ('2', 2),
+    ('3', 3),
+    ('4', 4),
+    ('5', 5),
+)
 class RestaurantForm(ModelForm):
-    class Meta:
+    class Meta: 
         model = Restaurant
         fields = ['name', 'address']
         labels = {
@@ -18,4 +26,22 @@ class RestaurantForm(ModelForm):
             'name': {
                 'max_length': _("이름이 너무 깁니다. 30자 이하로 해주세요."),
             },
+        }
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['point', 'comment', 'restaurant']
+        labels = {
+            'point': _('평점'),
+            'comment': _('코멘트'),
+        }
+        widgets = {
+            'restaurant': forms.HiddenInput(),  # 리뷰를 달 식당 정보는 사용자에게 보여지지 않도록 합니다.
+            'point': forms.Select(choices=REVIEW_POINT_CHOICES)  # 선택지를 인자로 전달합니다.
+        }
+        help_texts = {
+            'point': _('평점을 입력해주세요'),
+            'comment': _('코멘트를 입력해주세요'),
         }
